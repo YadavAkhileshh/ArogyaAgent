cff83403c3c8e54f9e12721d2027e0fb23c9094c
YadavAkhileshh <yadavakhil2501@gmail.com>
2025-12-06 12:01:15 +0530

    Fix Turbopack build error - use webpack flag

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.csv$/,
-      use: 'raw-loader'
-    });
-    return config;
-  }
+  turbopack: {}
 };
 
 module.exports = nextConfig;
