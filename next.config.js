89a4dbf7db90e973f155bb2080d33cfa83808fb8
YadavAkhileshh <yadavakhil2501@gmail.com>
2025-12-06 11:59:25 +0530

    Fix next.config warnings and update dependencies

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.csv': {
-          loaders: ['raw-loader'],
-          as: '*.js'
-        }
-      }
-    }
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.csv$/,
