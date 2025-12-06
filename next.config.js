2518dd73ea2fd928f7a34ca8355c0c1cb423117d
YadavAkhileshh <yadavakhil2501@gmail.com>
2025-12-06 11:57:09 +0530

    Fix deployment timeout - optimize vercel config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.csv': {
+          loaders: ['raw-loader'],
+          as: '*.js'
+        }
+      }
+    }
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.csv$/,
+      use: 'raw-loader'
+    });
+    return config;
+  }
+};
 
 module.exports = nextConfig;
